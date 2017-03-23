9902733976997f749ad6e268a2667b0a09b52ddc
Jinwon Kim <jinwkim90@gmail.com>
2017-03-23 14:14:38 +0900

    cc

---
+++ src/main/java/com/bit2017/mapreduce/join/JoinIDTitle.java
@@ -0,0 +1,96 @@
+package com.bit2017.mapreduce.join;
+
+import java.io.*;
+
+import org.apache.commons.logging.*;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.*;
+import org.apache.hadoop.mapreduce.lib.output.*;
+
+import com.bit2017.mapreduce.wordcount.*;
+
+public class JoinIDTitle {
+	private static Log log = LogFactory.getLog(SearchText.class);
+
+	public static class TitleDocIdMapper extends Mapper<Text, Text, Text, Text> {
+
+		@Override
+		protected void map(Text key, Text value, Mapper<Text, Text, Text, Text>.Context context) throws IOException, InterruptedException {
+		
+			context.write(value, new Text(key + "\t" + 1));
+			
+		}
+
+	}
+	
+	public static class DocIdCountMapper extends Mapper<Text, Text, Text, Text> {
+
+		@Override
+		protected void map(Text key, Text value, Mapper<Text, Text, Text, Text>.Context context) throws IOException, InterruptedException {
+			context.write(key, new Text(value + "\t" + 2));
+		}
+
+	}
+
+
+	public static class JobIdTitleReducer extends Reducer<Text, Text, Text, Text> {
+
+		@Override
+		protected void reduce(Text key, Iterable<Text> values,
+				Reducer<Text, Text, Text, Text>.Context context) throws IOException, InterruptedException {
+			int count = 0;
+			Text k = new Text();
+			Text v = new Text();
+			
+			for( Text value : values ) {
+				String info = value.toString();
+				String[] tokens = info.split( "\t" );
+				
+				if( "1".equals( tokens[1] ) ) {
+					k.set( tokens[0] + "[" + key.toString() + "]" );
+				} else if( "2".equals( tokens[1] ) ) {
+					v.set( tokens[0] );
+				}
+				
+				count++;
+			}
+			
+			//출력
+			if( count != 2 ) {
+				return;
+			}
+			
+			context.write( k,  v );
+		}
+	}
+
+	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+		Configuration conf = new Configuration();
+
+		Job job = new Job(conf, "Join ID & Title");
+		// 1. Job instance 초기화 과정
+		job.setJarByClass(JoinIDTitle.class);
+
+		// 파라미터 저장
+		final String TITLE_DOCID = args[0];
+		final String DOCID_COUNT = args[1];
+		final String OUTPUT_DIR = args[2];
+
+		/* 입력 관련 */
+		MultipleInputs.addInputPath(job, new Path(TITLE_DOCID), KeyValueTextInputFormat.class, TitleDocIdMapper.class );
+		MultipleInputs.addInputPath(job, new Path(DOCID_COUNT), KeyValueTextInputFormat.class, DocIdCountMapper.class  );
+		/* 출력 관련 */
+		job.setReducerClass(JobIdTitleReducer.class);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(Text.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		FileOutputFormat.setOutputPath(job, new Path(OUTPUT_DIR));
+
+		// 10. 실행
+		job.waitForCompletion(true);
+
+	}
+}
--- src/main/java/com/bit2017/mapreduce/topn/CountCitation.java
+++ src/main/java/com/bit2017/mapreduce/topn/CountCitation.java
@@ -12,105 +12,136 @@ import org.apache.hadoop.mapreduce.lib.output.*;
 
 import com.bit2017.mapreduce.topn.CountCitation.*;
 
-
 public class CountCitation {
 	public static class MyMapper extends Mapper<Text, Text, Text, LongWritable> {
-		
+
 		private Text word = new Text();
 		LongWritable one = new LongWritable(1L);
-
 
 		@Override
 		protected void map(Text key, Text value, Mapper<Text, Text, Text, LongWritable>.Context context)
 				throws IOException, InterruptedException {
-			/*log.info("-------------> map() called");*/
+			/* log.info("-------------> map() called"); */
 			String line = value.toString();
-			
-			/*	log.info("----------->tokenize worked");*/
-				word.set(line);
-				context.write(word, one);
-			}
 
-			
+			/* log.info("----------->tokenize worked"); */
+			word.set(line);
+			context.write(word, one);
+		}
+	}
+	
+	public static class MyMapper2 extends Mapper<Text, Text, Text, LongWritable> {
+
+		private Text word = new Text();
+		LongWritable one = new LongWritable(1L);
+
+		@Override
+		protected void map(Text key, Text value, Mapper<Text, Text, Text, LongWritable>.Context context)
+				throws IOException, InterruptedException {
+			/* log.info("-------------> map() called"); */
+			String line = value.toString();
+
+			/* log.info("----------->tokenize worked"); */
+			word.set(line);
+			context.write(word, one);
 		}
 
+	}
 
-		//run 은 보통 오버라이드를 하지 않음
-		/*	@Override
-				public void run(Mapper<LongWritable, Text, Text, LongWritable>.Context context)
-						throws IOException, InterruptedException {
-					// TODO Auto-generated method stub
-					super.run(context);
-				}
-		*/
-		
-	
 	public static class MyReducer extends Reducer<Text, LongWritable, Text, LongWritable> {
-		
+
 		private LongWritable sumWritable = new LongWritable();
-		
+
 		@Override
 		protected void reduce(Text key, Iterable<LongWritable> values,
-				Reducer<Text, LongWritable, Text, LongWritable>.Context context) throws IOException, InterruptedException {
-				
-		/*	long sum =0;
-			for(LongWritable value : values) {
-				sum += value.get();
-				log.info("------------>" + sum);
-			}*/
-			
+				Reducer<Text, LongWritable, Text, LongWritable>.Context context)
+				throws IOException, InterruptedException {
+
+			/*
+			 * long sum =0; for(LongWritable value : values) { sum +=
+			 * value.get(); log.info("------------>" + sum); }
+			 */
+
 			long unique = 0;
-			for(LongWritable value : values) {
-				unique +=value.get();
+			for (LongWritable value : values) {
+				unique += value.get();
 			}
-			
-			/*sumWritable.set(sum);*/
+
+			/* sumWritable.set(sum); */
 			sumWritable.set(unique);
-			//context.getCounter("Words Status", "Count of all Words").increment(sum);
+			// context.getCounter("Words Status", "Count of all
+			// Words").increment(sum);
 			context.getCounter("Words Status", "Count unique words").increment(unique);
-			
+
 			context.write(key, sumWritable);
-			
+
 		}
 	}
 
 	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
 		Configuration conf = new Configuration();
-		
+
 		Job job = new Job(conf, "CountCitation");
 		// 1. Job instance 초기화 과정
 		job.setJarByClass(CountCitation.class);
-		
-		//2. 맵퍼 클래스 지정
+
+		// 2. 맵퍼 클래스 지정
 		job.setMapperClass(MyMapper.class);
-		
-		//3. 리듀서 클래스 지정
+
+		// 3. 리듀서 클래스 지정
 		job.setReducerClass(MyReducer.class);
-		
-		//4. 출력키 타입
+
+		// 4. 출력키 타입
 		job.setMapOutputKeyClass(Text.class);
-		
-		//5. 출력밸류 타입
+
+		// 5. 출력밸류 타입
 		job.setMapOutputValueClass(LongWritable.class);
-		
-		//6. 입력파일 포맷 지정(생략)
+
+		// 6. 입력파일 포맷 지정(생략)
 		job.setInputFormatClass(KeyValueTextInputFormat.class);
-		
-		//7. 출력파일 포맷 지정(생략 가능)
+
+		// 7. 출력파일 포맷 지정(생략 가능)
 		job.setOutputFormatClass(TextOutputFormat.class);
-		
-		
-		//8.입력파일 이름 지정
+
+		// 8.입력파일 이름 지정
 		FileInputFormat.addInputPath(job, new Path(args[0]));
-		
-		//9.출력 디렉토리 지정gg
+
+		// 9.출력 디렉토리 지정gg
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		// 10. 실행
+		if (job.waitForCompletion(true) == false) {
+			return;
+		}
+
+		Configuration conf2 = new Configuration();
+		Job job2 = new Job(conf2, "Top N");
+
+		job2.setJarByClass(TopN.class);
+
+		job.setMapperClass(MyMapper2.class);
+
+		// 3. 리듀서 클래스 지정
+		job.setReducerClass(MyReducer.class);
+
+		// 4. 출력키 타입
+		job.setMapOutputKeyClass(Text.class);
+
+		// 5. 출력밸류 타입
+		job.setMapOutputValueClass(LongWritable.class);
+
+		// 6. 입력파일 포맷 지정(생략)
+		job.setInputFormatClass(KeyValueTextInputFormat.class);
+
+		// 7. 출력파일 포맷 지정(생략 가능)
+		job.setOutputFormatClass(TextOutputFormat.class);
+
+		// 8.입력파일 이름 지정
+		FileInputFormat.addInputPath(job2, new Path(args[1]));
+
+		// 9.출력 디렉토리 지정gg
+		FileOutputFormat.setOutputPath(job2, new Path(args[1] + "/topN"));
 		
-		
-		
-		//10. 실행
-		job.waitForCompletion(true);
-		
-		
+		job2.waitForCompletion(true);
 	}
 }
