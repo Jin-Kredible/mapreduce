a120b527d14860cf78fd83eef0cd218af091e2ba
Jinwon Kim <jinwkim90@gmail.com>
2017-03-23 14:58:58 +0900

    sdf

---
+++ src/main/java/com/bit2017/mapreduce/join/test.java
@@ -0,0 +1,18 @@
+package com.bit2017.mapreduce.join;
+
+public class test
+{
+    public static void main(String[] args)
+    {
+        String str = "The quick brown fox jumps over the lazy dog.";
+ 
+        // Get the index of all the characters of the alphabet
+        // starting from the beginning of the String.
+        int a = str.indexOf("o", 13);
+ 
+ 
+        // Display the results of all the indexOf method calls.
+        System.out.println( a);
+ 
+    }
+}
